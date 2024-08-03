b7a3cf6d155f7ee800c258e2c8dfa8c4c7b98e40
tsNedyalkova <ts.nedyalkova@abv.bg>
2024-08-04 01:00:41 +0300

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About VTarnovo Estate</h1>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum maiores expedita blanditiis cumque minus reiciendis molestiae dolore inventore impedit quaerat reprehenderit tenetur aperiam tempore veniam quod culpa eaque dicta facilis perspiciatis unde temporibus sapiente nihil, distinctio debitis. Sequi, natus esse?</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam ipsum consequatur minima vitae sequi in harum, optio assumenda veniam expedita dicta est soluta nobis sunt quia iste, commodi cumque placeat laboriosam molestiae eum veritatis sapiente modi! Perferendis quibusdam aspernatur sapiente.</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, eum. A dignissimos dolor amet ipsum distinctio, ab corrupti provident ut saepe. Eum harum maiores error ex, optio recusandae a eligendi, iste ullam dignissimos laborum illo quisquam assumenda vero consequatur sit?</p>
     </div>
   )
 }
